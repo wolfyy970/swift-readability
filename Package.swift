df2875600ae36677aad3b4e57a95fca1af7ebd88
KC <79471844+wolfyy970@users.noreply.github.com>
2026-07-19 21:18:51 +0700

    Make the package standalone and continuously verified

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(path: "../SwiftSoup")
+        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.13.5")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
